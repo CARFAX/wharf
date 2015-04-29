bbf7fc6f0e60c5d976ab4fe0fa660f399449cfcf
Matt Balmer <mattbalmer@carfax.com>
2015-04-29 16:21:22 -0500

    renderers

--- bin/wharf.js
+++ bin/wharf.js
@@ -1,14 +1,24 @@
 #!/usr/bin/env node
 
 var minimist = require('minimist'),
-    wharf = require('../lib/wharf'),
+    path = require('path'),
+    wharf = require('../lib'),
     options = minimist(process.argv.slice(2)),
     args = options._;
 delete options._;
 
+function paths(input, output) {
+    input = input || '';
+    output = output ? output : path.join(input, 'docs');  // default to input dir + '/docs'
+
+    return {
+        input: path.join(process.cwd(), input),
+        output: path.join(process.cwd(), output)
+    };
+}
+
 if(args[0] == 'compile') {
-    wharf.copyStaticFiles(process.cwd(), process.cwd() + '/docs', options);
-    wharf.compile(process.cwd(), process.cwd() + '/docs', options);
+    wharf.compile( paths(args[1], args[2]), options);
 } else {
     console.log('wharf - Node module documentation generator.', args, options);
 }
